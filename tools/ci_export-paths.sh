2aed4175a205e594bb751f0f2bdb4218fc495f75
Kuba <kuba.sejdak@gmail.com>
2018-02-08 19:02:34 +0100

    Fix CI.

--- tools/ci_export-paths.sh
+++ tools/ci_export-paths.sh
@@ -4,8 +4,8 @@ set -ev
 
 EXPORTS_FILE="~/path_exports"
 
-if [ ! -f ${EXPORTS_FILE} ]; then
-    echo "No ${EXPORTS_FILE} file found. Skipping."
+if [ ! -f "~/path_exports" ]; then
+    echo "No ~/path_exports file found. Skipping."
     exit 0
 fi
 
@@ -13,7 +13,7 @@ EXPORTED_PATHS=""
 while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
     echo "Read line ${LINE}"
     EXPORTED_PATHS=${LINE}:${EXPORTED_PATHS}
-done < ${EXPORTS_FILE}
+done < "~/path_exports"
 
 echo "EXPORTED_PATHS = ${EXPORTED_PATHS}"
 echo "export PATH=${EXPORTED_PATHS}:${PATH}" >> ~/.bash_profile
