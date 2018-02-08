37335cdbc05f963bd25d5d4a044db5dac4db0ff8
Kuba <kuba.sejdak@gmail.com>
2018-02-08 10:03:42 +0100

    Fix bad exporting PATH in cmake script.

--- tools/ci_install-cmake.sh
+++ tools/ci_install-cmake.sh
@@ -41,6 +41,6 @@ fi
 mkdir -p cmake
 tar --strip-components=1 -xf ${PACKAGE_BIN_NAME} -C cmake
 
-echo 'export PATH="${PWD}/cmake/bin:${PATH}"' >> ~/.bash_profile
+echo "export PATH=${PWD}/cmake/bin:${PATH}" >> ~/.bash_profile
 
 echo "Installing CMake v${VERSION} OK."
