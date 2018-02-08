1d268f30f252eaf7a8f3b3654c08289f2868893e
Kuba <kuba.sejdak@gmail.com>
2018-02-08 18:47:06 +0100

    Add logs to path exporting.

--- tools/ci_export-paths.sh
+++ tools/ci_export-paths.sh
@@ -11,7 +11,10 @@ fi
 
 EXPORTED_PATHS=""
 while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
+    echo "Read line ${LINE}"
     EXPORTED_PATHS=${LINE}:${EXPORTED_PATHS}
 done < ${EXPORTS_FILE}
 
+echo "EXPORTED_PATHS = ${EXPORTED_PATHS}"
 echo "export PATH=${EXPORTED_PATHS}:${PATH}" >> ~/.bash_profile
+cat ~/.bash_profile
