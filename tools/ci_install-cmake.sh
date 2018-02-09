d90f5e8646d76e6e860483da17bb94eb6a78529b
Kuba <kuba.sejdak@gmail.com>
2018-02-09 09:23:51 +0100

    Check cmake dir.

--- tools/ci_install-cmake.sh
+++ tools/ci_install-cmake.sh
@@ -42,7 +42,7 @@ mkdir -p cmake
 tar --strip-components=1 -xf ${PACKAGE_BIN_NAME} -C cmake
 
 ls -l
-ls -l ${PWD}/cmake/bin
+ls -l ${PWD}/cmake
 echo "${PWD}/cmake/bin" >> ~/path_exports
 
 echo "Installing CMake v${VERSION} OK."
