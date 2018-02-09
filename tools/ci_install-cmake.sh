f441ae96622ec15e0b682931913d1a9fd982421f
Kuba <kuba.sejdak@gmail.com>
2018-02-09 09:15:53 +0100

    Add logs to script installing clang.

--- tools/ci_install-cmake.sh
+++ tools/ci_install-cmake.sh
@@ -41,6 +41,8 @@ fi
 mkdir -p cmake
 tar --strip-components=1 -xf ${PACKAGE_BIN_NAME} -C cmake
 
+ls -l
+ls -l ${PWD}/cmake/bin
 echo "${PWD}/cmake/bin" >> ~/path_exports
 
 echo "Installing CMake v${VERSION} OK."
