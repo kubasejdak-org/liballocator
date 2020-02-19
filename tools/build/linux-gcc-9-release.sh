4c626cf6f4dd3711da05c776c90521b7561ad838
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-02-19 08:43:06 +0100

    Add basic CI.

--- tools/build/linux-gcc-9-release.sh
+++ tools/build/linux-gcc-9-release.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 conan install .. --build missing -pr gcc-9 -s build_type=Release
-cmake .. -DPLATFORM=linux -DCMAKE_BUILD_TYPE=Release
-make
+cmake .. -DPLATFORM=linux -DCMAKE_BUILD_TYPE=Release "${@}"
