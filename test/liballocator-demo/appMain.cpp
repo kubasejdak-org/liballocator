4b7ea685a9d741c5e36fcde164af8762d58946f9
Kuba Sejdak <kuba.sejdak@gmail.com>
2022-05-11 22:49:24 +0200

    Check CI 6.

--- test/liballocator-demo/appMain.cpp
+++ test/liballocator-demo/appMain.cpp
@@ -4,7 +4,7 @@
 /// @author Kuba Sejdak
 /// @copyright BSD 2-Clause License
 ///
-/// Copyright (c) 2017-2022, Kuba Sejdak <kuba.sejdak@gmail.com>
+/// Copyright (c) 2019-2022, Kuba Sejdak <kuba.sejdak@gmail.com>
 /// All rights reserved.
 ///
 /// Redistribution and use in source and binary forms, with or without
@@ -32,151 +32,19 @@
 
 #include "platformInit.hpp"
 
-#include <allocator/allocator.hpp>
+#include <fmt/printf.h>
 
-#include <cstdio>
 #include <cstdlib>
-#include <map>
-#include <memory>
-#include <string>
-#include <vector>
-
-// Defined in linker script.
-extern const char heapMin; // NOLINT
-extern const char heapMax; // NOLINT
-
-void* operator new(std::size_t size)
-{
-    return memory::allocator::allocate(size);
-}
-
-void operator delete(void* ptr) noexcept
-{
-    memory::allocator::release(ptr);
-}
-
-void operator delete(void* ptr, [[maybe_unused]] std::size_t sz) noexcept
-{
-    operator delete(ptr);
-}
-
-//static std::size_t freeMemory()
-//{
-//    return memory::allocator::getStats().freeMemorySize;
-//}
-//
-//static std::size_t initialFreeMemory;
-//
-//static void showStats()
-//{
-//    auto stats = memory::allocator::getStats();
-//    std::printf("Total memory size     : %u B\n", stats.totalMemorySize);
-//    std::printf("Reserved memory size  : %u B\n", stats.reservedMemorySize);
-//    std::printf("User memory size      : %u B\n", stats.userMemorySize);
-//    std::printf("Allocated memory size : %u B\n", stats.allocatedMemorySize);
-//    std::printf("Free memory size      : %u B\n", stats.freeMemorySize);
-//    std::printf("\n");
-//}
-
-//static void testStart(const char* description)
-//{
-//    std::printf("------------------------------------------------\n");
-//    std::printf("%s\n\n", description);
-//    showStats();
-//}
-//
-//static bool testEnd()
-//{
-//    showStats();
-//
-//    return (initialFreeMemory == freeMemory());
-//}
-
-//static bool testSmartPointers()
-//{
-//    testStart("Testing allocator with smart pointers");
-//
-//    {
-//        std::printf("Allocate 113 B (std::make_unique<char[]>)...\n");
-//        constexpr int cAllocSize = 113;
-//        auto ptr = std::make_unique<char[]>(cAllocSize); // NOLINT
-//        std::printf("ptr = %p\n", static_cast<void*>(ptr.get()));
-//        showStats();
-//
-//        std::printf("Release memory (ptr.reset())...\n");
-//    }
-//
-//    return testEnd();
-//}
-//
-//static bool testVector()
-//{
-//    testStart("Testing allocator with std::vector<int>");
-//
-//    {
-//        std::vector<int> vec;
-//        constexpr int cItemsCount = 16;
-//
-//        for (int i = 0; i < cItemsCount; ++i)
-//            vec.push_back(i);
-//
-//        for (std::size_t i = 0; i < vec.size(); ++i)
-//            std::printf("vec[%u] = %d\n", i, vec[i]);
-//    }
-//
-//    return testEnd();
-//}
-//
-//static bool testMap()
-//{
-//    testStart("Testing allocator with std::map<int, std::string>");
-//
-//    {
-//        std::map<int, std::string> map;
-//        map[0] = "Great resources on modern C++:";
-//        map[1] = "C++ Weekly - YouTube channel hosted by Jason Turner";
-//        map[2] = "\"Effective\" books by Scott Meyers";
-//        map[3] = "C++ Core Guidelines - set of guidelines, rules, and best practices about coding in C++";
-//        map[4] = "CppCast - the first podcast for C++ developers by C++ developers";
-//
-//        for (const auto& [key, value] : map)
-//            std::printf("map[%d] = %s\n", key, value.c_str());
-//    }
-//
-//    return testEnd();
-//}
 
 // NOLINTNEXTLINE
-int appMain([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+int appMain(int argc, char* argv[])
 {
     if (!platformInit())
         return EXIT_FAILURE;
 
-//    constexpr std::size_t cPageSize = 512;
-//    if (!memory::allocator::init(std::uintptr_t(&heapMin), std::uintptr_t(&heapMax), cPageSize)) {
-//        std::printf("error: Failed to initialize liballocator.\n");
-//        std::printf("FAILED\n");
-//        return EXIT_FAILURE;
-//    }
-//
-//    std::printf("Initialized liballocator v%s.\n\n", memory::allocator::version());
-//    initialFreeMemory = freeMemory();
-//
-//    if (!testSmartPointers()) {
-//        std::printf("FAILED\n");
-//        return EXIT_FAILURE;
-//    }
-//
-//    if (!testVector()) {
-//        std::printf("FAILED\n");
-//        return EXIT_FAILURE;
-//    }
-//
-//    if (!testMap()) {
-//        std::printf("FAILED\n");
-//        return EXIT_FAILURE;
-//    }
+    for (int i = 0; i < argc; ++i)
+        fmt::print("argv[{}] = '{}'\n", i, argv[0]);
 
-    std::printf("PASSED\n");
+    fmt::print("PASSED\n");
     return EXIT_SUCCESS;
 }
